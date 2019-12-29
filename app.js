4ea7db994679c9d4a3dfb75dc6f0fd548a0c8891
SuperZheng <zhengbin@aossci.com>
2019-12-29 15:06:13 +0800

    新增 样式

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
+  onLaunch: function (options) {
     console.log("app onLaunch 小程序初始化完成 ")
+    console.log(options);
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
+    console.log(options);
     console.log('app onShow 小程序从【后台】进入【前台】')
   },
 
@@ -25,6 +27,24 @@ App({
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    console.log('app onError 全局调用发生错误')
+    console.log('app onError 全局调用发生错误: '+ msg)
+  },
+
+  /**
+   * 页面不存在监听函数
+   * 小程序要打开不存在的页面不存在时触发 
+   */
+  onPageNotFound:function(options) {
+    console.log('onPageNotFound执行');
+    console.log(options);
+  },
+
+  // 全局变量
+  num : 123,
+
+ // 全局方法函数 
+  test : function() {
+    console.log('test');
   }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,13 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    result : '',
+    result2 : ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      console.log('index页面'+options);
       console.log("index onLoad 监听页面加载");
   },
 
@@ -19,7 +21,14 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log("index onReady 监听页面初次渲染完成")
+    // 获得全局 app
+    var app = getApp();
+    // 调用全局变量
+    console.log(app.num);
+    // 调用全局方法
+    app.test();
+    
+    console.log("index onReady 监听页面初次渲染完成");
   },
 
   /**
@@ -62,5 +71,70 @@ Page({
    */
   onShareAppMessage: function () {
     console.log("index onShareAppMessage 用户点击右上角分享")
-  }
+  },
+  /**
+   *  当页面有滚动会触发此函数
+   * @param {*} options 
+   */
+  onPageScroll :function(options) {
+    console.log('此时用户正在滚动页面');
+    console.log('滚动距离：'+ options.scrollTop)
+  },
+
+  /**
+   * 按钮绑定的函数
+   */
+  compare: function (e) {
+    console.log('111按钮被点击了');
+    console.log(e);
+    var str = "两数相等";
+    if(this.num1 > this.num2) {
+      str = '第 1 个数大';
+    } else if (this.num2 > this.num1) {
+      str = "第 2 个数大";
+    }
+    // 将比较结果 赋值 过去
+    // 这里不能使用 this.data.result = str 这种方式改变页面中的 {{result}} 的值
+    // 需要通过 this.setData() 方法来实现，该方法的参数是一个对象，传入{result : str} 就是将页面中 {{result}} 的值改变变量 str 的值
+    this.setData({result : str})
+  },
+
+  num1 : 0,
+  num2 : 0,
+  num3 : 0,
+  num4 : 0,
+  // 输入框事件,监听第一数字输入框
+  num1change : function(e) {
+    console.log(e);
+    this.num1 = Number (e.detail.value);
+    console.log('第一个数为：'+ this.num1);
+  },
+  // 输入框事件,监听第二数字输入框
+  num2change : function(e) {
+    this.num2 = Number (e.detail.value);
+    console.log('第二个数为：'+ this.num2);
+  },
+
+ // 比较两个数的大小
+ // 通过 "e.target.dataset.名称"  的方式 可以获取 "data-名称" 的值
+  change : function(e) {
+    this[e.target.dataset.id] = Number(e.detail.value);
+    console.log('输入的数字为：'+ this[e.target.dataset.id])
+  },
+
+  compare2: function (e) {
+    console.log('222按钮被点击了');
+    console.log(e);
+    var str = "两数相等";
+    if(this.num3 > this.num4) {
+      str = '第 1 个数大';
+    } else if (this.num4 > this.num3) {
+      str = "第 2 个数大";
+    }
+    // 将比较结果 赋值 过去
+    // 这里不能使用 this.data.result = str 这种方式改变页面中的 {{result}} 的值
+    // 需要通过 this.setData() 方法来实现，该方法的参数是一个对象，传入{result : str} 就是将页面中 {{result}} 的值改变变量 str 的值
+    this.setData({result2 : str})
+  },
+
 })
